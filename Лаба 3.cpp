083851893dfa381664bacf6e92f550f0f3c1ddf5
zandell <70910663+zandell@users.noreply.github.com>
2020-10-26 13:22:20 +0300

    Create Лаба 3.cpp

---
+++ Лаба 3.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cmath>
+
+double krk1(double x,double y){
+double a = sin(x) + cos(y):
+double b = cos(x) - sin(y);
+retutn (a/b) * tan(x*y);
+}
+
+double lls2(double x, double y){
+printf("сумма %u/n",x + y);
+printf("разность %u/n", x - y);
+printf("частное %u", x / y);
+}
+
+int main()
+printf("Формула %u", formula_1(1, 2));
+printf("%U", TASK_7(1, 2));
+RETURN 0:
+}
